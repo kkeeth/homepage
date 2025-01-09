579869e9cc0a717177b49366521c13b5e8c64af1
kkeeth <zensin0082@gmail.com>
2025-01-09 23:12:44 +0900

    style: hero section layout

--- src/utils/shoelace-components.js
+++ src/utils/shoelace-components.js
@@ -5,6 +5,7 @@ import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
 import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
 import '@shoelace-style/shoelace/dist/components/drawer/drawer.js';
+import '@shoelace-style/shoelace/dist/components/divider/divider.js';
 
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
 setBasePath(
