24818e70078e670e49007d8a8f98b7d7fc822dbc
kkeeth <zensin0082@gmail.com>
2025-01-21 23:21:16 +0900

    feat: i18n about works

---
+++ src/locales/dataset.js
@@ -0,0 +1,73 @@
+export const works = {
+  en: [
+    {
+      title: 'Engineering Manager',
+      description: 'Engineering Manager (Full-time) at Kaminashi Inc.',
+      image: '/images/work_kaminashi.jpeg',
+      tags: ['Management', 'Team Building', 'Recruiting', 'Technical Branding'],
+      link: 'https://corp.kaminashi.jp/',
+    },
+    {
+      title: 'Programming Teacher',
+      description:
+        'Teaching JavaScript/React as a programming instructor at a private school called KADOKAWA Dwango Institute of Information Technology.',
+      image: '/images/work_vantan.png',
+      tags: ['JavaScript', 'React', 'Firebase'],
+      link: 'https://kadokawadwango-it-academy-hs.jp/',
+    },
+    {
+      title: 'Advisory for new graduate engineer recruitment/engineer training',
+      description:
+        'Through the medium of EXPERT NETWORK, we recruit new graduate engineers and provide advisory services for engineering education programs.',
+      image: '/images/myface.png',
+      tags: [
+        'Recruitment of new graduates',
+        'Training of Engineers',
+        'Advisory',
+      ],
+      link: 'https://web.expert-network.io/',
+    },
+    {
+      title: 'Advertisement placement in podcast programs',
+      description:
+        'Advertising placement on my engineering-related podcast program, and podcast program production.',
+      image: '/images/work_kkeeth.png',
+      tags: ['Podcasts', 'Technical Branding'],
+      link: 'https://art19.com/shows/kkeethengineers',
+    },
+  ],
+  ja: [
+    {
+      title: 'エンジニアリングマネージャー',
+      description:
+        '株式会社カミナシで正社員として Engineering Manager をしております．',
+      image: '/images/work_kaminashi.jpeg',
+      tags: ['マネジメント', 'チームビルディング', '採用', '技術広報'],
+      link: 'https://corp.kaminashi.jp/',
+    },
+    {
+      title: 'プログラミングの講師',
+      description:
+        'KADOKAWAドワンゴ情報工科学院という私学校で，プログラミングの講師として JavaScript/React を教えています．',
+      image: '/images/work_vantan.png',
+      tags: ['JavaScript', 'React', 'Firebase'],
+      link: 'https://kadokawadwango-it-academy-hs.jp/',
+    },
+    {
+      title: '新卒エンジニア採用/エンジニア教育のアドバイザリー',
+      description:
+        'EXPERT NETWORK という媒体経由で，新卒エンジニア採用や，エンジニア教育プログラムのアドバイザリーを行っています．',
+      image: '/images/myface.png',
+      tags: ['新卒採用', 'エンジニア教育', 'アドバイザリー'],
+      link: 'https://web.expert-network.io/',
+    },
+    {
+      title: 'ポッドキャスト番組への広告出稿',
+      description:
+        '私が配信しているエンジニアリング関連のポッドキャスト番組への広告出稿，ポッドキャスト番組制作を行っています．',
+      image: '/images/work_kkeeth.png',
+      tags: ['ポッドキャスト', '音声配信', '技術広報'],
+      link: 'https://art19.com/shows/kkeethengineers',
+    },
+  ],
+};
