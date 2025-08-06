f5111e6e656cc35be0c7163f6f45f5f9afc18582
kkeeth <zensin0082@gmail.com>
2025-08-06 10:37:05 +0900

    feat: Date formatting functions modularized into utils

---
+++ src/utils/formatDate.js
@@ -0,0 +1,8 @@
+export const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return date.toLocaleDateString('ja-JP', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+}
