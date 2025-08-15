62b80af108128ea3b235ff5c6851e2b5392347d9
kkeeth <zensin0082@gmail.com>
2025-08-16 01:06:49 +0900

    style: re-design

--- src/locales/dataset.js
+++ src/locales/dataset.js
@@ -1,33 +1,6 @@
 export const works = {
   en: [
     {
-      title: 'Engineering Manager',
-      description: 'Engineering Manager (Full-time) at Kaminashi Inc.',
-      image: '/images/work_kaminashi.jpeg',
-      tags: ['Management', 'Team Building', 'Recruiting', 'Technical Branding'],
-      link: 'https://corp.kaminashi.jp/',
-    },
-    {
-      title: 'Programming Teacher',
-      description:
-        'Teaching JavaScript/React as a programming instructor at a private school called KADOKAWA Dwango Institute of Information Technology.',
-      image: '/images/work_vantan.png',
-      tags: ['JavaScript', 'React', 'Firebase'],
-      link: 'https://kadokawadwango-it-academy-hs.jp/',
-    },
-    {
-      title: 'Advisory for new graduate engineer recruitment/engineer training',
-      description:
-        'Through the medium of EXPERT NETWORK, we recruit new graduate engineers and provide advisory services for engineering education programs.',
-      image: '/images/myface.png',
-      tags: [
-        'Recruitment of new graduates',
-        'Training of Engineers',
-        'Advisory',
-      ],
-      link: 'https://web.expert-network.io/',
-    },
-    {
       title: 'Advertisement placement in podcast programs',
       description:
         'Advertising placement on my engineering-related podcast program, and podcast program production.',
@@ -37,30 +10,6 @@ export const works = {
     },
   ],
   ja: [
-    {
-      title: 'エンジニアリングマネージャー',
-      description:
-        '株式会社カミナシで正社員として Engineering Manager をしております．',
-      image: '/images/work_kaminashi.jpeg',
-      tags: ['マネジメント', 'チームビルディング', '採用', '技術広報'],
-      link: 'https://corp.kaminashi.jp/',
-    },
-    {
-      title: 'プログラミングの講師',
-      description:
-        'KADOKAWAドワンゴ情報工科学院という私学校で，プログラミングの講師として JavaScript/React を教えています．',
-      image: '/images/work_vantan.png',
-      tags: ['JavaScript', 'React', 'Firebase'],
-      link: 'https://kadokawadwango-it-academy-hs.jp/',
-    },
-    {
-      title: '新卒エンジニア採用/エンジニア教育のアドバイザリー',
-      description:
-        'EXPERT NETWORK という媒体経由で，新卒エンジニア採用や，エンジニア教育プログラムのアドバイザリーを行っています．',
-      image: '/images/myface.png',
-      tags: ['新卒採用', 'エンジニア教育', 'アドバイザリー'],
-      link: 'https://web.expert-network.io/',
-    },
     {
       title: 'ポッドキャスト番組への広告出稿',
       description:
--- src/utils/truncateText.js
+++ src/utils/truncateText.js
@@ -1,4 +1,4 @@
-export const truncateText = (text, limit) => {
+export const truncateText = (text, limit = 120) => {
   if (text.length <= limit) return text;
   return text.substring(0, limit).trim() + '...';
 }
