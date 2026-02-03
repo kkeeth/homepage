4750ced5d9d66a4411b58c103fc7fb6a838dc578
kkeeth <zensin0082@gmail.com>
2026-02-04 00:35:23 +0900

    feat: add footer links

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import '@riotjs/hot-reload';
 import { install, component } from 'riot';
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-setBasePath(
-  'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/',
-);
+
+// Shoelace (設定はshoelace-components.jsに一本化)
+import '@/utils/shoelace-components';
 
 import '@/assets/style.css';
-import '@/utils/shoelace-components';
 import App from '@/app.riot';
 import registerGlobalComponents from '@/register-global-components';
 import i18nStore from '@/stores/i18n-store';
